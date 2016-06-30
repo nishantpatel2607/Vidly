9c4bacc2270f9720611788200fe92523347e95d5
nishantpatel2607 <npatel2607@gmail.com>
2016-06-30 15:17:21 +1000

    CustomerDto

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter customer's name")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool isSubsribedToNewsLetter { get; set; }
+        
+        public byte MembershipTypeId { get; set; } //EF will treat as foreign key
+
+        //[Min18YearsIfMember]
+        public DateTime? BirthDate { get; set; }
+    }
+}
